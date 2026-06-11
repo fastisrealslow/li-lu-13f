8be7e42a1cedffef12441a5faf8e2f5129b69978
fastisrealslow <156089984+fastisrealslow@users.noreply.github.com>
2026-06-11 16:19:44 +0800

    fix: allTime unit detection

--- fetch_prices_akre_greenberg.py
+++ fetch_prices_akre_greenberg.py
@@ -209,6 +209,9 @@ def process_investor(name, data_file, prices_file):
                         q_price = low * 0.7 + avg * 0.3
                     else:
                         q_price = qd["value"] / qd["shares"]
+                        # SEC pre-2023 value field is in thousands; detect and correct
+                        if q_price > 5000:
+                            q_price = q_price / 1000
                     total_weighted_cost += q_price * qd["shares"]
                     total_shares_sum += qd["shares"]
                     valid_q += 1
